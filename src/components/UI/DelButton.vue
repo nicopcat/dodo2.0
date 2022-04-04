9c71f6b76363c5ec37e3cf1e4a5ca609d07ca985
nicopcat <tianfang07@outlook.com>
2022-04-04 18:29:16 +0800

    ???

--- src/components/UI/DelButton.vue
+++ src/components/UI/DelButton.vue
@@ -7,18 +7,18 @@
 <style scoped>
 span {
   float: right;
-  padding: 0.2em 0.3em;
+  padding: 0.2em 0.2em;
   height: 1em;
   line-height: 1.1em;
   cursor: pointer;
   border-radius: 4px;
   color: rgb(252, 252, 252);
-  background-color: rgba(118, 118, 118, 0.932);
+  background-color: rgba(226, 226, 226, 0.898);
   transition: all 0.5s;
   transform: scale(0.7);
 }
 
 span:hover {
-  background-color: rgba(122, 122, 122, 0.571);
+  background-color: rgba(229, 229, 229, 0.465);
 }
 </style>
