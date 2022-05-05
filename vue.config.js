9c83709757804ed585267619956202a5288f9c48
nic <ying_12004@126.com>
2022-05-05 19:45:25 +0800

    icon

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,11 @@ module.exports = {
   assetsDir: "static",
   productionSourceMap: false,
   pwa: {
-    favicon32: "./favicon-32x32.png",
-    favicon16: "./favicon-16x16.png",
-    appleTouchIcon: "./apple-touch-icon.png",
-    maskIcon: "./favicon.svg",
-    msTileImage: "./apple-touch-icon.png",
+    faviconSVG: "img/icons/favicon.svg",
+    favicon32: "img/icons/favicon-32x32.png",
+    favicon16: "img/icons/favicon-16x16.png",
+    appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+    maskIcon: "img/icons/safari-pinned-tab.svg",
+    msTileImage: "img/icons/msapplication-icon-144x144.png",
   },
 };
