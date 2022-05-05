433e7febb91908e8dbf968a21f048fa3d7671678
nic <ying_12004@126.com>
2022-05-05 16:34:05 +0800

    icon update.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dodo-v2/'
-    : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/dodo-v2/" : "/",
   transpileDependencies: true,
-  assetsDir: 'static',
+  assetsDir: "static",
   productionSourceMap: false,
   pwa: {
-    iconPaths: {
-      favicon32: './favicon-32x32.png',
-      favicon16: './favicon-16x16.png',
-      appleTouchIcon: './apple-touch-icon.png',
-      maskIcon: './favicon.svg',
-      msTileImage: './apple-touch-icon.png'
-    }
-  }
-}
+    favicon32: "./favicon-32x32.png",
+    favicon16: "./favicon-16x16.png",
+    appleTouchIcon: "./apple-touch-icon.png",
+    maskIcon: "./favicon.svg",
+    msTileImage: "./apple-touch-icon.png",
+  },
+};
